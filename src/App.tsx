94517cda71a93ad8c9ff0c47194306fb4ecbd485
Niklavs Romans <niklavsromans19@gmail.com>
2022-05-01 00:44:38 +0200

    default files

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
+import Planner1 from './components/planner1/Planner1';
+import Planner2 from './components/planner2/Planner2';
+import Planner3 from './components/planner3/Planner3';
+import Planner4 from './components/planner4/Planner4';
 
 const App = () => (
   <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit
-        {' '}
-        <code>src/App.tsx</code>
-        {' '}
-        and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
+    <h2>Todos</h2>
+    <br />
+    <Planner1 />
+    <br />
+    <Planner2 />
+    <br />
+    <Planner3 />
+    <br />
+    <Planner4 />
+    <br />
   </div>
 );
 
